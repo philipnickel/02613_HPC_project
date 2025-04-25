523ae4c79f31e455d4b7f71854f977701e97d30c
kasper1616 <s214786@gmail.com>
2025-04-25 10:38:54 +0200

    jacobi_numba

--- Python_scripts/Funcs/jacobi.py
+++ Python_scripts/Funcs/jacobi.py
@@ -2,10 +2,11 @@ from os.path import join
 import sys
 
 import numpy as np
+from numba import jit
 
 #@profile
 def jacobi(u, interior_mask, max_iter, atol=1e-6):
-   #u = np.copy(u)
+    u = np.copy(u)
 
     for i in range(max_iter):
         # Compute average of left, right, up and down neighbors, see eq. (1)
@@ -31,3 +32,19 @@ def jacobi_optimized(u, interior_mask, max_iter, atol=1e-6):
         if delta < atol:
             break
     return u
+
+@jit(nopython=True)
+def jacobi_cuda(u, interior_mask, max_iter, atol=1e-6):
+    #u = np.copy(u)
+
+    for i in range(max_iter):
+        # Compute average of left, right, up and down neighbors, see eq. (1)
+        u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
+        u_new_interior = u_new[interior_mask]
+        delta = np.abs(u[1:-1, 1:-1][interior_mask] - u_new_interior).max()
+        u[1:-1, 1:-1][interior_mask] = u_new_interior
+
+        if delta < atol:
+            break
+    return u
+
