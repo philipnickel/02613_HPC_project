55161c999ce0d2824ec2d5d1b53e24b1eda4a2ac
kasper1616 <s214786@gmail.com>
2025-04-25 12:15:14 +0200

    Added numba + numba parallel

--- Python_scripts/Funcs/jacobi.py
+++ Python_scripts/Funcs/jacobi.py
@@ -2,7 +2,7 @@ from os.path import join
 import sys
 
 import numpy as np
-from numba import jit
+from numba import jit, njit, prange
 
 #@profile
 def jacobi(u, interior_mask, max_iter, atol=1e-6):
@@ -33,36 +33,69 @@ def jacobi_optimized(u, interior_mask, max_iter, atol=1e-6):
             break
     return u
 
-@jit(nopython=True)
-def jacobi_numba(u, interior_mask, max_iter, atol=1e-6):
-    #u = np.copy(u)
-    shape = u.shape
-    u_flat = u.ravel()
-    n = shape[1]
 
-    for i in range(max_iter):
-        # Compute average of left, right, up and down neighbors, see eq. (1)
-        u_new = np.empty((shape[0]-2) * (shape[1]-2))
-        for j in range(1, shape[0]-1):
-            for k in range(1, shape[1]-1):
-                idx = j*n + k
-                u_new[(j-1)*(shape[1]-2) + (k-1)] = 0.25 * (
-                    u_flat[idx-1] +      # left
-                    u_flat[idx+1] +      # right 
-                    u_flat[idx-n] +      # up
-                    u_flat[idx+n]        # down
-                )
-        
-        u_new_interior = u_new[interior_mask]
-        interior_indices = np.where(interior_mask)[0]
-        flat_indices = np.array([(j+1)*n + (k+1) for j,k in 
-                                enumerate(interior_indices//(shape[1]-2))])
-        
-        delta = np.abs(u_flat[flat_indices] - u_new_interior).max()
-        u_flat[flat_indices] = u_new_interior
+def jacobi_numba(u, interior_mask, max_iter, atol=1e-6, parallel=False):
+    ys, xs = np.where(interior_mask)
+    if parallel: 
+        return numba_helper_parallel(u, ys, xs, max_iter, atol)
+    else: 
+        return numba_helper(u, ys, xs, max_iter, atol)
+
+@njit(parallel=True)
+def compute_u_new_parallel(u, u_new):
+    nx, ny = u.shape
+    for i in prange(1, nx - 1):  # prange enables parallel execution
+        for j in range(1, ny - 1):
+            u_new[i - 1, j - 1] = 0.25 * (
+                u[i, j - 1] + u[i, j + 1] + u[i - 1, j] + u[i + 1, j]
+            )
+
+
+@njit
+def numba_helper(u, ys, xs, max_iter, atol=1e-6):
+    u = u.copy()
+    
+    for _ in range(max_iter):
+        #if parallel:
+            #u_new = compute_u_new_parallel(u)
+
+        #else:
+        u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
+
+        delta = 0.0
+        for k in range(len(ys)):
+            i = ys[k]
+            j = xs[k]
+            diff = abs(u[i + 1, j + 1] - u_new[i, j])
+            if diff > delta:
+                delta = diff
+            u[i + 1, j + 1] = u_new[i, j]
 
         if delta < atol:
             break
-            
-    return u_flat.reshape(shape)
 
+    return u
+
+@njit
+def numba_helper_parallel(u, ys, xs, max_iter, atol=1e-6):
+    u = u.copy()
+    nx, ny = u.shape
+    u_new = np.empty((nx - 2, ny - 2))
+    
+    for _ in range(max_iter):
+
+        compute_u_new_parallel(u, u_new)
+
+        delta = 0.0
+        for k in range(len(ys)):
+            i = ys[k]
+            j = xs[k]
+            diff = abs(u[i + 1, j + 1] - u_new[i, j])
+            if diff > delta:
+                delta = diff
+            u[i + 1, j + 1] = u_new[i, j]
+
+        if delta < atol:
+            break
+
+    return u
